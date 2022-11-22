e59950f1c72e3420308a8c34d8b3575562303400
Ajay Chauhan <arachauhan@gmail.com>
2022-11-22 11:18:25 +0530

    Update run.py
    added one more visualisation

--- app/run.py
+++ app/run.py
@@ -43,6 +43,11 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # category data for plotting
+    categories =  df[df.columns[4:]]
+    category_counts = (categories.mean()*categories.shape[0]).sort_values(ascending=False)
+    category_names = list(category_counts.index)
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,6 +68,26 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+         # category plotting
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+            
         }
     ]
     
